7a6ed1a08011f92f8ae65f16a43e64045a78ed3a
alexandre <alexandre020602@gmail.com>
2026-02-27 11:21:21 -0300

    creating a route architecture for the project

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express"
+import routes from "./routes/index.js"
 
 const app = express()
 
+app.use(express.json())
+app.use(routes)
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+
+export default app
