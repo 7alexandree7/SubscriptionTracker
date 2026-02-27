676c21adf1a76c0aad2ae865e07e9730ed6e0db4
alexandre <alexandre020602@gmail.com>
2026-02-27 12:05:23 -0300

    adding PORT from env file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import app from "./app.js"
+import { PORT } from "./config/env.js"
 
 async function startServer() {
     try {
-        const server = app.listen(3000, () => {
-            console.log("Server is running on port 3000")
+        const server = app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
         })
 
         server.on("error", (error) => {
