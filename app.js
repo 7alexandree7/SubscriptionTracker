2b98717f6e5dce1b620a34a3bfacbc88edd4fff8
alexandre <alexandre020602@gmail.com>
2026-02-28 14:01:39 -0300

    creating e adding errorMiddleware and outhers middleware in app

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from "express"
 import routes from "./routes/index.js"
+import cookieParser from "cookie-parser"
+import errorMiddleware from "./middlewares/error.middleware.js"
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 app.use("/api/v1", routes)
+
+app.use(errorMiddleware)
 
 
 export default app
