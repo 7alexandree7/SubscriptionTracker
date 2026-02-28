9134803d5e482d85362c0b1efa7421e90e735b91
alexandre <alexandre020602@gmail.com>
2026-02-28 11:09:11 -0300

    creating a model for user with mongoose

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "User name is required"],
+        trim: true,
+        minLength: 2,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        required: [true, "User email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 2,
+        maxLength: 100,
+        match: [/\S+@\S+\.\S+/, "Please use a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "User password is required"],
+        minLength: 6,
+    }
+}, {
+    timestamps: true
+});
+
+const user = mongoose.model("User", userSchema)
+
+export default user
