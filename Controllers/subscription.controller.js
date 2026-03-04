ab0566e7e9ececd71efb1f1891499a478f94a42e
alexandre <alexandre020602@gmail.com>
2026-03-04 14:05:56 -0300

    Add update and delete subscription functionality; update routes and API documentation

--- Controllers/subscription.controller.js
+++ Controllers/subscription.controller.js
@@ -51,7 +51,7 @@ export const getAllSubscriptions = async (req, res, next) => {
             message: "All subscriptions retrieved successfully",
             data: allSubscriptions
         })
-        
+
     } catch (error) {
         next(error)
     }
@@ -60,9 +60,9 @@ export const getAllSubscriptions = async (req, res, next) => {
 
 export const getSubscriptionsDetails = async (req, res, next) => {
     try {
-        const subscriptionDetails  = await Subscription.findById(req.params.id)
+        const subscriptionDetails = await Subscription.findById(req.params.id)
 
-        if(!subscriptionDetails) {
+        if (!subscriptionDetails) {
             return res.status(404).json({
                 success: false,
                 message: "Subscription not found"
@@ -73,6 +73,48 @@ export const getSubscriptionsDetails = async (req, res, next) => {
             success: true,
             message: "Subscription details retrieved successfully",
             data: subscriptionDetails
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const updateSubscription = async (req, res, next) => {
+    try {
+
+        const updateSubscription = await Subscription.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        if (!updateSubscription) {
+            return res.status(404).json({
+                success: false,
+                message: "Subscription not found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "Subscription updated successfully",
+            data: updateSubscription
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const deleteSubscription = await Subscription.findByIdAndDelete(req.params.id)
+        if (!deleteSubscription) {
+            return res.status(404).json({
+                success: false,
+                message: "Subscription not found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "Subscription deleted successfully",
+            data: deleteSubscription
         })
     } catch (error) {
         next(error)
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express"
-import { createSubscription, getSubscriptions, getAllSubscriptions, getSubscriptionsDetails } from "../Controllers/subscription.controller.js"
+import {
+    createSubscription,
+    getSubscriptions,
+    getAllSubscriptions,
+    getSubscriptionsDetails,
+    updateSubscription,
+    deleteSubscription
+} from "../Controllers/subscription.controller.js"
 import authorize from "../middlewares/auth.middleware.js"
 
 const subscriptionRouter = Router()
 
-subscriptionRouter.post("/", authorize ,createSubscription)
+subscriptionRouter.post("/", authorize, createSubscription)
 subscriptionRouter.get("/user/:id", authorize, getSubscriptions)
 subscriptionRouter.get("/all", authorize, getAllSubscriptions)
 subscriptionRouter.get("/details/:id", authorize, getSubscriptionsDetails)
+subscriptionRouter.put("/:id", authorize, updateSubscription)
+subscriptionRouter.delete("/:id", authorize, deleteSubscription)
 
 export default subscriptionRouter;
